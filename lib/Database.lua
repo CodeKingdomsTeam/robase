9c9b4bd2aa6a6c9ecaf31fd33f1aec78a40e7faa
Jed Palmer <jed@codekingdoms.com>
2018-09-03 14:36:59 +0100

    Add initial documentation, and changelog

--- lib/Database.lua
+++ lib/Database.lua
@@ -1,75 +1,160 @@
+--[[
+	Roblox's DataStoreService is how you can save and load information about your game
+	and players permanently - across servers. For full information on the DataStoreService,
+	please take a look here: http://wiki.roblox.com/index.php?title=API:Class/DataStoreService 
+]]
+
+-- Create return table
 local Database = {}
+
+-- Import libraries and helper functions
 local Logger = require(script.Parent.Logger)
+local DataStoreService = game:GetService("DataStoreService")
 
-local backend = game:GetService("DataStoreService")
+--[[
+	Saves a key, value set for the specified player in Roblox's DataStoreService. Player's
+	data is split down by scope, so you do not have to worry about safely storing different
+	player information together. 
 
+	Note that this function does not return anything, and will not immediately save, as it 
+	is an async call. 
+]]
 --- @ServerOnly
 function Database.SavePlayerData(player, storeName, keyName, data) --: <T extends Serializable>(Player, string, string, T) => void
+
+	-- The scope is automatically defined, as this function saves player-specific data
 	local scope = "player_" .. player.UserId
- 	local ok, output = pcall(function()
-		backend:GetDataStore(storeName, scope):UpdateAsync(keyName, function(oldValue)
+
+	-- Saving data isn't surefire, so a pcall check is required
+	 local ok, output = pcall(function()
+
+		-- Retrieve the data store, and update - note that this is asynchronous 
+		DataStoreService:GetDataStore(storeName, scope):UpdateAsync(keyName, function(oldValue)
+
+			-- If there wasn't an previous value, player data is being created
 			if(oldValue == nil) then
 			Logger.Debug("Database: Creating player data for ", player.Name,  " at ", storeName, ".", keyName, " with value of ", data)
+
 			else
+				-- Else it is being updated
 				Logger.Debug("Database: Updating player data for ", player.Name,  " at ", storeName, ".", keyName, " from ", oldValue, " to ", data)
 			end 
+
+			-- UpdateAsync requires data to be returned
 			return data
 		end) 
 	end)
+
+	-- Should it not successfully save the player data, throw an error
  	if not ok then
 		Logger.Error("Database load error at ", scope, ".", storeName, ".", keyName, ". Stacktrace: ", output)
 	end
  end
 
+ --[[
+	Loads the value of a key for the specified player in Roblox's DataStoreService. Player's
+	data is split down by scope, so you do not have to worry about safely storing different
+	player information together. 
+
+	Note that this function returns async, so do not expect to immediately get the data.
+]]
 --- @ServerOnly
 function Database.LoadPlayerData(player, storeName, keyName) --: <T extends Serializable>(Player, string, string) => T
+	
+	-- The scope is automaticall defined, as this function loads player-specific data
 	local scope = "player_" .. player.UserId
+
+	-- If this is not overridden, the function will return nil
 	local response = nil
+
+	-- Loading from data store can throw errors, so need to catch and deal with them properly
  	local ok, output = pcall(function()
-		Logger.Debug("Database: Getting player data for ", player.Name, " at ", storeName, ".", keyName)		
-		response = backend:GetDataStore(storeName, scope):GetAsync(keyName)
+		Logger.Debug("Database: Getting player data for ", player.Name, " at ", storeName, ".", keyName)
 		
+		-- Get the key from the data store - is async. Will be nil if it cannot get the data
+		response = DataStoreService:GetDataStore(storeName, scope):GetAsync(keyName)
+		
+		-- This means there is no data with the specified keyName
 		if response == nil then
 			Logger.Warn("Database: Could not get data from ", scope, ".", storeName, ".", keyName)
 		end 
 	end)
+
+	-- If the load request threw an error, print it out here so it can be fixed
 	if not ok then
 		Logger.Error("Database load error at ", scope, ".", storeName, ".", keyName, ". Stacktrace: ", output)
 	end
+
+	-- Will either be nil, or the data
  	return response
 end
 
+ --[[
+	Saves a key, value set for the game in Roblox's DataStoreService. This is intended for saving
+	data such as monsters left, and is not per player. Take care you are not overwriting previous
+	data within the same keyName that you do not want to. 
+
+	Note that this function does not return anything, and will not immediately save, as it 
+	is an async call. 
+]]
 --- @ServerOnly
 function Database.SaveGameData(storeName, keyName, data) --: <T extends Serializable>(string, string, T) => void
- 	local ok, output = pcall(function()
-		backend:GetDataStore(storeName, "game"):UpdateAsync(keyName, function(oldValue)
+
+	-- Saving to the data store can 
+	 local ok, output = pcall(function()
+		
+		-- Retrieve the data store, and update - note that this is asynchronous 
+		DataStoreService:GetDataStore(storeName, "game"):UpdateAsync(keyName, function(oldValue)
+
+			-- This means new data is being created
 			if(oldValue == nil) then
             Logger.Debug("Database: Creating game data at ", storeName, ".", keyName, " with value of ", data)
 			else
+				-- Else existing data is being overwritten
 				Logger.Debug("Database: Updating game data at ", storeName, ".", keyName, " from ", oldValue, " to ", data)
 			end 
+
+			-- Roblox function requires this to be returned
 			return data
 		end) 
- 	end)
+	 end)
+	 
+	-- If there was an error while getting the data, catch and log it
  	if not ok then
 		Logger.Error("Database save error at game.", storeName, ".", keyName, ". Stacktrace: ", output)
 	end
  end
 
+--[[
+	Loads the value of a key for the game in Roblox's DataStoreService. This is intended for 
+	loading data such as monsters left, and is not per player.
+
+	Note that this function returns async, so do not expect to immediately get the data.
+]]
 --- @ServerOnly
 function Database.LoadGameData(storeName, keyName) --: <T extends Serializable>(string, string) => T
+	-- If this is not overridden, the function will return nil
 	local response = nil
+
+	-- Loading from data store can throw errors, so need to catch and deal with them properly
  	local ok, output = pcall(function()
-		Logger.Debug("Database: Getting game data at ", storeName, ".", keyName)		
-		response = backend:GetDataStore(storeName, "game"):GetAsync(keyName)
+		Logger.Debug("Database: Getting game data at ", storeName, ".", keyName)
 		
+		-- Get the key from the data store - is async. Will be nil if it cannot get the data
+		response = DataStoreService:GetDataStore(storeName, "game"):GetAsync(keyName)
+		
+		-- This means there is no data with the specified keyName
 		if response == nil then
 			Logger.Debug("Database: Could not get data from ", scope, ".", storeName, ".", keyName)
 		end 
 	end)
+
+	-- If the load request threw an error, print it out here so it can be fixed
 	if not ok then
 		Logger.Error("Database load error at game.", storeName, ".", keyName, ". Stacktrace: ", output)
 	end
+
+	-- Will either be nil, or the data
  	return response
 end
 
--- lib/Input.lua
+++ lib/Input.lua
@@ -1,76 +1,120 @@
+--[[
+	This deals with all input from a user to their client. Including mouse, keyboard, and other platforms that
+	Roblox runs on. For additional reading, it is recommended that you take a look at the input section of 
+	the Roblox API reference: https://www.robloxdev.com/api-reference/class/Mouse.
+]]
+
+-- Create return table
+local Input = {}
+
+-- Import libraries and helper functions
 local Logger = require(script.Parent.Logger)
 local Types = require(script.Parent.Types)
 local Events = require(script.Parent.Events)
 local Type = Types.Type
-local Input = {}
 local ContextActionService = game:GetService("ContextActionService")
 
+-- Create a ButtonEventMap for the virtual button system
 Input.ButtonEventMap = Type("ButtonEventMap", {             --: <T>{[event:string]: Events.EventHandler<T>}
     OnPressed = Type("OnPressed"),          				--: <this T>() => void
     OnReleased = Type("OnReleased"),						--: <this T>() => void
 })
 
+-- Create a table to hold all the virtual buttons
 local buttonsByName = {}
 
---- Add a virtual button bound to provided input keys
+--[[
+	This creates a virtual button, allowing multiple different Roblox inputs to be bound to the same
+	'button'. It acts like a base Roblox button, heavily reducing the amount of code required to make 
+	a game cross-platform compatible. 
+]]
 --- @ClientOnly
 function Input.AddButton( buttonName, events, ... ) --: string, ButtonEventMap, ...KeyCode => void
 
+	-- Creates the virtual button object
 	local button = {
 		Name = buttonName,
 		Events = events,
 		Keys = {...},
 		IsPressed = false
 	}
+
+	-- Places the button into the table of buttons
 	buttonsByName[buttonName] = button
 
+	-- Creates a local function that will be fired by Roblox's ActionService when the state of the button changes
 	local OnInput = function( buttonName, inputState )
 	
+		-- If the button has just been pressed
 		if inputState == Enum.UserInputState.Begin then
 			
+			-- Set the button object to be pressed
 			button.IsPressed = true
+
+			-- If the button has an OnPressed event, fire it for the local player
 			if ( button.Events.OnPressed ) then 
 				button.Events.OnPressed( game.Players.LocalPlayer, button, buttonName )
 			end
 	
+		-- Else if the button has just stopped being pressed
 		elseif inputState == Enum.UserInputState.End then
 	
+			-- Set the button object to no longer be pressed
 			button.IsPressed = false
+
+			-- If the button has an OnReleased event, fire it for the local player
 			if ( button.Events.OnReleased ) then 
 				button.Events.OnReleased( game.Players.LocalPlayer, button, buttonName )
 			end
 	
 		else
-	
+			-- Not an input state we want!
 			Logger:Warn("Unexpected input state for button " .. buttonName .. ": " .. inputState)
-	
+
 		end
 	
 	end
 
+	-- Bind the above function to all the different unput types
 	ContextActionService:BindAction(buttonName, OnInput, true, ...)
 
 end
 
---- Remove a virtual button
+--[[
+	This removes a virtual button that has been created.
+]]
 --- @ClientOnly
 function Input.RemoveButton(buttonName)
 
-	buttonsByName[buttonName] = nil
+	-- If the virtual button exists, delete it
+	if(buttonsByName[buttonName])then
+
+		buttonsByName[buttonName] = nil
+
+	else
+
+		Logger:Warn("Tried to remove button " .. buttonName .. ", but it does not exist!")
+	end 
 
 end
 
---- Check if a virtual button is pressed
+--[[
+	Returns true or false depending on if the specified virtual button is pressed
+]]
 --- @ClientOnly
 function Input.IsPressed( buttonName )
 
+	-- Check that the button exists first of all
 	if ( not buttonsByName[buttonName] ) then
 
 		Logger:Warn("Button doesn't exist " .. buttonName)
+
+		-- If the button doesn't exist, then it cannot be pressed
 		return false
 
 	end
 
+	-- Return 
 	return buttonsByName[buttonName].IsPressed
 	
 end
