e5a41b65180c8a6eb5ec307da5bef98db591ae33
Andrew Moss <andrew@codekingdoms.com>
2018-09-06 12:00:07 +0100

    add infrastructure for lemur tests

---
+++ spec/Example_spec.lua
@@ -0,0 +1,27 @@
+describe(
+    "Proof of Concept Robase test",
+    function()
+        package.path = package.path .. ";./?/init.lua"
+        local lemur = require("modules.lemur")
+
+        it(
+            "test 1",
+            function()
+                -- Create a Habitat
+                local habitat = lemur.Habitat.new()
+                local ReplicatedStorage = habitat.game:GetService("ReplicatedStorage")
+
+                -- Load `src/roblox` as a Folder containing some ModuleScripts:
+                local Api = habitat:loadFromFs("./lib")
+                Api.Parent = ReplicatedStorage
+                Api.Name = "Api"
+
+                local Objects = habitat:require(Api.Objects)
+                Objects.Create("TextLabel")
+
+                local label = habitat.game.Workspace:FindFirstChildOfClass("TextLabel")
+                assert(label ~= nil)
+            end
+        )
+    end
+)
