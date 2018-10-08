dff95a0d46aadb8e6e402c980977168c7dfc90e9
Andrew Moss <andrew@codekingdoms.com>
2018-10-08 10:52:17 +0100

    add assertions for debounce usage

--- lib/Utils.lua
+++ lib/Utils.lua
@@ -4,6 +4,9 @@ local Utils = {}
 	Creates a debounced function that delays invoking fn until after delayS milliseconds have elapsed since the last time the debounced function was invoked.
 ]]
 function Utils.Debounce(fn, delayS)
+	assert(type(fn) == "function" or (type(fn) == "table" and getmetatable(fn) and getmetatable(fn).__call ~= nil))
+	assert(type(delayS) == "number")
+
 	local lastInvocation = 0
 	local lastResult = nil
 
